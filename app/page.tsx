6336981fe26e97c5eadf41e42e2396a4860064bd
Brian <punongbayan.bri@gmail.com>
2025-04-08 14:36:09 +0800

    fix(page): disable other pages for testing

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,22 @@
 import { ReactLenis } from 'lenis/react';
 
-import About from './(sections)/about';
-import Experience from './(sections)/experience';
+// import About from './(sections)/about';
+// import Experience from './(sections)/experience';
 import Hero from './(sections)/hero';
 import PreLoader from './(sections)/preloader';
-import Projects from './(sections)/projects';
-import Works from './(sections)/works';
+
+// import Projects from './(sections)/projects';
+// import Works from './(sections)/works';
 
 export default function Home() {
   return (
     <ReactLenis root>
       <PreLoader />
       <Hero />
-      <About />
+      {/* <About />
       <Experience />
       <Works />
-      <Projects />
+      <Projects /> */}
     </ReactLenis>
   );
 }
