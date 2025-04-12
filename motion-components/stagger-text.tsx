b3c65928e27bb8bc9df6d734b1acbf8660d99254
Brian <punongbayan.bri@gmail.com>
2025-04-12 20:51:36 +0800

    fix(stagger-text): decrease duration

--- motion-components/stagger-text.tsx
+++ motion-components/stagger-text.tsx
@@ -25,7 +25,7 @@ const individualLetter = (quick: boolean) => {
         // ease: [0.6, 0.01, -0.05, 0.95],
         // ease: [0.76, 0, 0.24, 1],
         ease: [0.65, 0, 0.35, 1],
-        duration: quick ? 0.4 : 1.2,
+        duration: quick ? 0.4 : 0.8,
       },
     },
   };
